6439aabd6409961fe402f3eba9941cadfaeb9fc4
Darrel Kathan <darrel.kathan@grainger.com>
2019-08-17 18:41:34 -0500

    Fixed App class

--- lib/App.php
+++ lib/App.php
@@ -1,7 +1,7 @@
 <?php
 
 class App{
-	public static connect(){
+	public static function connect(){
 		$db = pg_connect("host=$_ENV[RDS_HOSTNAME] port=$_ENV[RDS_PORT] dbname=$_ENV[RDS_DB_NAME] user=$_ENV[RDS_USERNAME] password=$_ENV[RDS_PASSWORD]");
 		if($db){
 			echo "Success";
