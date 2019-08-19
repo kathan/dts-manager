f19f22c15ddb87a0515dd6a35c461d238ce31727
Darrel Kathan <darrel.kathan@grainger.com>
2019-08-19 16:43:49 -0500

    Fixed db connection

--- includes/DB.php
+++ includes/DB.php
@@ -2,15 +2,15 @@
 
 class DB{
     public static $db;
-    public static function connect($dbuser, $dbpass, $dbname='', $dbhost='localhost', $port=5432){
-		if(!isset(self::$db)){
-			self::$db = new mysqli ($dbhost, $dbuser, $dbpass, $dbname);
+    public static function connect($dbuser, $dbpass, $dbname='', $dbhost='localhost'){
+	if(!isset(self::$db)){
+            self::$db = new mysqli ($dbhost, $dbuser, $dbpass, $dbname);
             if(!self::$db){
                 return false;
             }
-		}
+	}
 		
-		return self::$db;
+	return self::$db;
     }
 	
     public static function close(){
@@ -18,11 +18,11 @@ class DB{
     }
 	
     public static function query($sql, $limit=null, $start=null){
-		if(isset($limit)){
+	if(isset($limit)){
             isset($start) ? $sql .= " LIMIT $start" : $sql .= " LIMIT 0";
             $sql .= ", $limit";
-		}
-		return self::$db->query($sql);
+	}
+	return self::$db->query($sql);
     }
 	
     public static function num_rows($result){
@@ -32,63 +32,63 @@ class DB{
     	return 0;
     }
 	
-	public static function numrows($result){
+    public static function numrows($result){
         return self::num_rows($result);
-	}
+    }
 
-	public static function result($result, $row, $field){
+    public static function result($result, $row, $field){
         $result->data_seek($row);
         $row_obj = $result->fetch_row();
         return $row_obj[$field];
-	}
+    }
 	
-	public static function affected_rows(){
+    public static function affected_rows(){
         return self::$db->affected_rows;
-	}
+    }
 	
-	public static function fetch_array($result){
+    public static function fetch_array($result){
         return $result->fetch_array();
-	}
+    }
 
-	public static function fetch_assoc($result){
+    public static function fetch_assoc($result){
         return $result->fetch_assoc();
-	}
+    }
 	
-	public static function insertid(){
+    public static function insertid(){
         return self::$db->insert_id();
-	}
+    }
 	
-	public static function last_insert_id(){
+    public static function last_insert_id(){
         return self::insertid();
-	}
+    }
 
-	public static function error(){
+    public static function error(){
         if(self::$db->error){
 			return self::$db->error;
         }
-	}
+    }
 
-	public static function current_db(){
-		$sql = "SELECT database()";
-		$re = DB::query($sql);
-		$row = DB::fetch_array($result);
-		return $row[0];
-	}
+    public static function current_db(){
+	$sql = "SELECT database()";
+	$re = DB::query($sql);
+	$row = DB::fetch_array($result);
+	return $row[0];
+    }
 	
-	public static function form_to_db($s){
+    public static function form_to_db($s){
         return self::$db->real_escape_string($s);
-	}
+    }
 
-	public static function db_date($year, $month, $day){
+    public static function db_date($year, $month, $day){
         return $year.'-'. str_pad ($month, 2, '0', STR_PAD_LEFT).'-'.str_pad ($day, 2, '0', STR_PAD_LEFT);
-	}
+    }
 	
-	public static function date_to_mySQL($origdate=null){
+    public static function date_to_mySQL($origdate=null){
         if(isset($origdate)){
             return date("Y-m-d H:i:s", strtotime($origdate));
         }
         return date("Y-m-d H:i:s");
-	}
+    }
 	
     public static function esc($str){
         return self::$db->real_escape_string($str);
--- includes/app.php
+++ includes/app.php
@@ -1,6 +1,7 @@
 <?php
 ini_set('error_reporting', E_ALL);
-error_reporting(E_ALL & ~E_NOTICE & ~E_DEPRECATED);
+error_reporting(E_ALL);
+//error_reporting(E_ALL & ~E_NOTICE & ~E_DEPRECATED);
 date_default_timezone_set('America/Chicago');
 //==========App Settings===========
 define('SITE_NAME', 'DTS');
@@ -12,49 +13,50 @@ define('PRIVATE_ROOT', "$_SERVER[HTTP_HOST]/members");
 //=================================
 
 class App{
-	public static function getTempDir(){
-		return self::getAppRoot().self::$templates_dir;
-	}
+    public static $db;
+    public static $templates_dir = '/templates/';
+
+    public static function getTempDir(){
+        return self::getAppRoot().self::$templates_dir;
+    }
 	
-	public static function getAppRoot(){
-		return pathinfo(__DIR__, PATHINFO_DIRNAME);
-	}
+    public static function getAppRoot(){
+        return pathinfo(__DIR__, PATHINFO_DIRNAME);
+    }
+
+    public static function get_username($user_id){
+        $sql = "SELECT username
+		FROM users
+		WHERE user_id = $user_id";
+        $re = DB::query($sql);
+	if(DB::num_rows($re) > 0){
+            $r = DB::fetch_assoc($re);
+            return $r['username'];
+            }
+    }
 	
-	public static $templates_dir = '/templates/';
-	public static function get_username($user_id){
-		$sql = "SELECT username
-				FROM users
-				WHERE user_id = $user_id";
-		$re = DB::query($sql);
-		if(DB::num_rows($re) > 0)
-		{
-			$r = DB::fetch_assoc($re);
-			return $r['username'];
+    public static function dbConnect(){
+	if(!isset(self::$db)){
+            self::$db = mysqli_init();
+            $result = DB::connect($_ENV['RDS_USERNAME'], $_ENV['RDS_PASSWORD'], $_ENV['RDS_DB_NAME'], $_ENV['RDS_HOSTNAME']);
+            if($result){
+                echo "connected";
+                if(!DB::query( "SET NAMES 'utf8'")){
+                    return false;
 		}
-	}
+            }else{
+		echo self::$db->error;
+		return false;
+            }
+        }
+	return true;
+    }
 	
-	public static function dbConnect(){
-		if(!isset(self::$db)){
-			self::$db = mysqli_init();
-			$result = mysqli_real_connect(self::$db, $_ENV['RDS_HOSTNAME'], $_ENV['RDS_USERNAME'], $_ENV['RDS_PASSWORD'], 'dts');
-			if($result){
-			  echo "connected";
-			  if(!mysqli_query(self::$db, "SET NAMES 'utf8'")){
-			  	return false;
-			  }
-			}else{
-			  echo mysqli_connect_error();
-			  return false;
-			}
-		}
-		return true;
+    public static function init(){
+	if(!self::dbConnect()){
+            echo "Could not connect to database.";
 	}
-	
-	public static function init(){
-		if(!self::dbConnect()){
-			echo "Could not connect to database.";
-		}
-	}
+    }
 }
 
 App::init();
