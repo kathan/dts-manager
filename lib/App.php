7f59109924c2b9933b853d372b25056d749bf35e
Darrel Kathan <darrel.kathan@grainger.com>
2019-08-19 12:53:35 -0500

    Fixed connect call

--- lib/App.php
+++ lib/App.php
@@ -2,8 +2,9 @@
 
 class App{
 	public static function connect(){
-		$db = pg_connect("host=$_ENV[RDS_HOSTNAME] port=$_ENV[RDS_PORT] dbname=$_ENV[RDS_DB_NAME] user=$_ENV[RDS_USERNAME] password=$_ENV[RDS_PASSWORD]");
-		if($db){
+            $db = new mysqli ($_ENV['RDS_HOSTNAME'], $_ENV['RDS_USERNAME'], $_ENV['RDS_PASSWORD'], $_ENV['RDS_DB_NAME']);
+//            $db = pg_connect("host=$_ENV[RDS_HOSTNAME] port=$_ENV[RDS_PORT] dbname=$_ENV[RDS_DB_NAME] user=$_ENV[RDS_USERNAME] password=$_ENV[RDS_PASSWORD]");
+            if($db){
 			echo "Success";
 		}else{
 			echo "Failed";
