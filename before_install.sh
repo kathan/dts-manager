f4ed233daea32ff5e26b25a32c030ec3968222f7
Darrel Kathan <darrel.kathan@grainger.com>
2019-08-19 13:17:38 -0500

    Moved phpMyAdmin install to before_install.sh

---
+++ before_install.sh
@@ -0,0 +1,3 @@
+curl -O https://files.phpmyadmin.net/phpMyAdmin/4.9.0.1/phpMyAdmin-4.9.0.1-all-languages.zip
+unzip phpMyAdmin-4.9.0.1-all-languages.zip
+mv phpMyAdmin-4.9.0.1-all-languages phpMyAdmin
