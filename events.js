ef67025a3519ca45444fa17f56a60d98257c092a
Darrel Kathan <darrel.kathan@grainger.com>
2019-08-19 22:17:07 -0500

    Cleanup

--- events.js
+++
@@ -1,5 +0,0 @@
-<?php
-echo "function row_clicked(id, pk, name){
-	window.location=\"?action=Edit&page=\"+name+\"&\"+pk+\"=\"+id;
-}";
-?>
--- events.js.php
+++ events.js.php
@@ -1,8 +1,7 @@
 <?php
 require_once('includes/app.php');
-echo "function row_clicked(id, pk, name)
-{
+echo "function row_clicked(id, pk, name){
 	//window.alert(name);
-	window.location=\"http://".HTTP_ROOT."/?action=Edit&page=\"+name+\"&\"+pk+\"=\"+id
+	window.location=\"?action=Edit&page=\"+name+\"&\"+pk+\"=\"+id
 }";
 ?>
